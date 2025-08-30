2564fdf92e90c4393908a3ed70d7eb1e5bcef784
Tomasz-Gziut <tomasz.g.direct@gmail.com>
2025-08-31 00:51:42 +0200

    /fix onwer data display

--- src/components/Owner.tsx
+++ src/components/Owner.tsx
@@ -9,31 +9,39 @@ export default function Owner() {
   }
 
   return (
-    <div className="bg-[rgba(0,0,0,1)] text-white flex justify-center p-20 items-center">
+    <div className="bg-black text-white flex flex-col md:flex-row justify-center p-4 md:p-10 w-full gap-6 break-words items-center">
       <img
-        className="rounded-full w-50 h-50 "
+        className="rounded-full w-40 h-40"
         src={owner.avatar_url}
         alt={owner.name}
       />
-      <div className="flex flex-col justify-center p-10 gap-4">
+
+      <div className="flex flex-col gap-4 max-w-full">
         <div className="flex flex-col">
-          <p className="text-xl">{owner.name}</p>
-          <p className="text-gray-400">creator of {owner.company}</p>
+          <p className="text-xl font-semibold">{owner.name}</p>
+          <p className="text-gray-400 text-sm">creator of {owner.company}</p>
         </div>
 
-        <p className="text-gray-400">{owner.bio}</p>
+        <p className="text-gray-400 break-words">{owner.bio}</p>
         <p className="text-gray-400">Country: {owner.location}</p>
-        <p className="text-gray-400">{owner.twitter_username}</p>
 
-        <div className="w-200 text-gray-400">{owner.readme}</div>
-        <div className="flex gap-2 items-center">
-          <div className="flex flex-row gap-1">
-            <MailIcon></MailIcon>
+        <div className="text-gray-400 break-words max-w-200">
+          {owner.readme}
+        </div>
+
+        <div className="flex gap-2 flex-wrap items-center">
+          <div className="flex gap-1 items-center">
+            <MailIcon />
             {owner.email}
           </div>
-          <p>{owner.twitter_username}</p>
-          <a href={owner.html_url}>
-            <GitHubIcon size={24} color="white"></GitHubIcon>
+          <p className="text-gray-400">{owner.twitter_username}</p>
+          <a
+            className="flex flex-row gap-1"
+            href={owner.html_url}
+            target="_blank"
+          >
+            <GitHubIcon size={24} color="white" />
+            Github
           </a>
         </div>
       </div>
