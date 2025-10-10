ad9187308af55cb941178c2026b81de0ecf058fa
Tomasz-Gziut <225775888+Tomasz-Gziut@users.noreply.github.com>
2025-10-10 23:17:32 +0200

    /feature added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/old" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
