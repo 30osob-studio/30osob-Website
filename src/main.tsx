87e1f8109e29cc096cbb2079ddca817bf8857722
Tomasz Gziut <tomasz.g.direct@gmail.com>
2025-10-15 13:07:16 +0200

    Revert "V.0.5"

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./Home.tsx";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-ix11.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  }
+  plugins: [react()],
 })
