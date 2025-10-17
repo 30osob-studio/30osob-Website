39b517a3cfbae9b91b117024308fe374459595ae
Tomasz-Gziut <tomasz.g.direct@gmail.com>
2025-10-17 13:36:37 +0200

    refractor(components): merged OwnerProjectsBar and ProjectsBar as one ProjectsCounter

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,35 @@ import About from "./components/About";
 import ProjectList from "./components/ProjectList";
 import Owner from "./components/Owner";
 import Header from "./components/Header";
-import ProjectsBar from "./components/ProjectsBar";
+import ProjectsCounter from "./components/ProjectsCounter";
 import Footer from "./components/Footer";
-import OwnerProjectsBar from "./components/OwnerProjectsBar";
 import { useRepos } from "./hooks/useRepos";
 import { useOwnerRepos } from "./hooks/useOwnerRepos";
+import { useAbout } from "./hooks/useAbout";
+import { useOwner } from "./hooks/useOwner";
 
 function App() {
   const { repos, fallbackText: reposFallbackText } = useRepos();
   const { repos: ownerRepos, fallbackText: ownerReposFallbackText } = useOwnerRepos();
+  const { about, fallbackText: aboutFallbackText } = useAbout();
+  const { owner, fallbackText: ownerFallbackText } = useOwner();
 
   return (
     <>
       <Header />
       <About />
-      <ProjectsBar></ProjectsBar>
+      <ProjectsCounter
+        count={about?.public_repos}
+        fallbackText={aboutFallbackText}
+        className="bg-black text-white p-8"
+      />
       <ProjectList projects={repos} fallbackText={reposFallbackText} />
       <Owner />
-      <OwnerProjectsBar></OwnerProjectsBar>
+      <ProjectsCounter
+        count={owner?.public_repos}
+        fallbackText={ownerFallbackText}
+        className="bg-black text-white p-8 border-t border-gray-400"
+      />
       <ProjectList projects={ownerRepos} fallbackText={ownerReposFallbackText} />
       <Footer></Footer>
     </>
