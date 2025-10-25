342bb0faaf738da06b99c84e3e176c5dc26e15a2
Tomasz-Gziut <225775888+Tomasz-Gziut@users.noreply.github.com>
2025-10-25 02:50:04 +0200

    fix(components): Cleaning Project component code structure

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -45,7 +45,7 @@ export default function Project({ project }: ProjectProps) {
               rel="noopener noreferrer"
             >
               <CardTitle className="text-[clamp(2rem,2vw,2rem)] font-bold text-black break-words w-full">
-                {project.name.replace(/-/g, ' ')}
+                {project.name.replace(/-/g, " ")}
               </CardTitle>
             </a>
           </CardAction>
@@ -61,31 +61,44 @@ export default function Project({ project }: ProjectProps) {
           <div className="border-t-2 border-gray-400 border-dotted -mx-12"></div>
           <div className="flex justify-between items-center">
             <span className="text-black">
-              <p className="font-bold text-[clamp(1rem,1vw,1rem)]">Created at:</p>
-              <p className="text-black text-sm lato-regular text-[clamp(0.9rem,0.9vw,0.9rem)]">{creationDate}</p>
+              <p className="font-bold text-[clamp(1rem,1vw,1rem)]">
+                Created at:
+              </p>
+              <p className="text-black text-sm lato-regular text-[clamp(0.9rem,0.9vw,0.9rem)]">
+                {creationDate}
+              </p>
             </span>
             <span className="text-black">
-              <p className="font-bold text-[clamp(1rem,1vw,1rem)]">Last change:</p>
-              <p className="text-black text-sm lato-regular text-[clamp(0.9rem,0.9vw,0.9rem)]">{updatedDate}</p>
+              <p className="font-bold text-[clamp(1rem,1vw,1rem)]">
+                Last change:
+              </p>
+              <p className="text-black text-sm lato-regular text-[clamp(0.9rem,0.9vw,0.9rem)]">
+                {updatedDate}
+              </p>
             </span>
           </div>
-          <div className="border-t-2 border-gray-400 border-dotted -mx-12"></div>   
-          <TagList items={project.languages} tagClassName="bg-black text-white text-[clamp(0.9rem,0.9vw,0.9rem)]" />
-          <TagList items={project.topics} tagClassName="bg-white text-black text-[clamp(0.9rem,0.9vw,0.9rem)]" />
+          <div className="border-t-2 border-gray-400 border-dotted -mx-12"></div>
+          <TagList
+            items={project.languages}
+            tagClassName="bg-black text-white text-[clamp(0.9rem,0.9vw,0.9rem)]"
+          />
+          <TagList
+            items={project.topics}
+            tagClassName="bg-white text-black text-[clamp(0.9rem,0.9vw,0.9rem)]"
+          />
         </CardContent>
         <div className="border-t-3 border-black"></div>
         <CardFooter className="flex justify-between items-center mt-3">
           <a href={project.html_url} target="_blank" rel="noopener noreferrer">
-            <GitHubIcon size={37.6} color="black"/>
-            
+            <GitHubIcon size={37.6} color="black" />
           </a>
           {project.contributors && project.contributors.length > 0 && (
-            <div className="border-3 black rounded-full p-[0.15rem]"><Contributors contributors={project.contributors} /></div>
+            <div className="border-3 black rounded-full p-[0.15rem]">
+              <Contributors contributors={project.contributors} />
+            </div>
           )}
-          
         </CardFooter>
       </div>
-      
     </Card>
   );
 }
